09bc4d666ba2f0093839badaaa841e76ebb3dd8d
gmorae <gmmartins@stefanini.com>
2020-03-18 22:17:29 -0300

    criação de rota e componente junto com o json-server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule),
+  },
+  {
+    path: 'create',
+    loadChildren: () => import('./pages/form/form.module').then(m => m.FormModule)
+  },
+  {
+    path: 'update',
+    loadChildren: () => import('./pages/form/form.module').then(m => m.FormModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
